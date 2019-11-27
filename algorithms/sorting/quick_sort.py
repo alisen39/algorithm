d9335e2cefcf0757669e1b9a793a6ddd0049758a
alisen <alisen39@163.com>
2019-11-27 23:36:11 +0800

    增加选择排序

---
+++ algorithms/sorting/quick_sort.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# author:alisen
+# time: 2019-11-27 21:25
+
+'''快速排序'''
+
+
---
+++ algorithms/sorting/selectionSort.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# author:alisen
+# time: 2019-11-27 21:29
+
+'''
+    选择排序
+        最原始最传统的排序方法 时间复杂度 O(n^2) 空间复杂度 O(n)
+
+    1. 遍历列表，找出最小的一个数
+        保存第一个数，与后面的数依次对比，遇到比自己小的便记录下索引与数值
+    2. 将最小的数添加到新的列表中，同时删除原数组中的数
+    3. 重复第一步
+'''
+
+
+class Solution:
+    def selection(self, disorder_arr: list):
+        if len(disorder_arr) <= 0:
+            return []
+
+        result_arr = []
+        for _ in range(len(disorder_arr)):
+
+            min_num = disorder_arr[0]
+            num_index = 0
+
+            for index, item in enumerate(disorder_arr):
+                if item < min_num:
+                    min_num = item
+                    num_index = index
+
+            disorder_arr.pop(num_index)
+            result_arr.append(min_num)
+
+        return result_arr
+
+
+if __name__ == '__main__':
+    # arr = []
+    # arr = [1]
+    # arr = [2, 1]
+    # arr = [1, 2]
+    arr = [92, 1, 21, 10, 0, -10, -1, 101, 2, 2, 10]
+
+    res = Solution().selection(arr)
+    print(res)
