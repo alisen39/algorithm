d0da701cf4a089be32b2a78154f3b73e32f622d6
alisen <alisen39@163.com>
2019-12-23 23:45:50 +0800

    新增解法

--- algorithms/removeDuplicates.py
+++ algorithms/removeDuplicates.py
@@ -13,20 +13,32 @@
 
 
 class Solution:
-    def removeDuplicates(self, nums: list):
-        p = 1
-        while p < len(nums):
-            if nums[p] == nums[p - 1]:
-                nums.pop(p)
-            else:
-                p += 1
+    # def removeDuplicates(self, nums: list):
+    #     p = 1
+    #     while p < len(nums):
+    #         if nums[p] == nums[p - 1]:
+    #             nums.pop(p)
+    #         else:
+    #             p += 1
+    #
+    #     print(nums)
+    #     return len(nums)
 
-        print(nums)
-        return len(nums)
+    def removeDuplicates(self, nums: list):
+        p = 0
+        for i in range(1, len(nums) ):
+            if nums[i] != nums[p]:
+                p += 1
+                nums[p] = nums[i]
+
+        # nums = nums[0:p+1]
+        # print(nums)
+        return p+1
 
 
 if __name__ == '__main__':
     # arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
     arr = [0, 0, 0]
 
-    Solution().removeDuplicates(arr)
+    res = Solution().removeDuplicates(arr)
+    print(res)
