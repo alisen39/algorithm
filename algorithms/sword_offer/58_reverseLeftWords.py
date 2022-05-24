14a4b5571ac6a165acc024758cf6c973a6afafde
Alisen <alisen39@163.com>
2022-05-24 20:03:19 +0800

    58 左旋转字符串  切片法

---
+++ algorithms/sword_offer/58_reverseLeftWords.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# author: Alisen
+# time: 2022/5/24 20:00
+# desc:
+""" 剑指 Offer 58 - II. 左旋转字符串
+https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+"""
+
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        return s[n:] + s[:n]
+
+
+if __name__ == '__main__':
+    s = 'abcdefg'
+    n = 2
+    res = Solution().reverseLeftWords(s, n)
+    print(res)
