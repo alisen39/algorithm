061200994ba3315e4bd98efdf71da45786db2f89
Alisen <alisen39@163.com>
2022-07-07 23:19:13 +0800

    斐波那契 循环解法

--- algorithms/sword_offer/10_fib.py
+++ algorithms/sword_offer/10_fib.py
@@ -11,17 +11,34 @@ from functools import lru_cache
 
 
 class Solution:
-    @lru_cache()
+    # @lru_cache()
+    # def fib(self, n: int) -> int:
+    #     n = n % 1000000007
+    #     if n == 0:
+    #         return 0
+    #     elif n == 1:
+    #         return 1
+    #     else:
+    #         return self.fib(n - 1) + self.fib(n - 2)%1000000007
+
     def fib(self, n: int) -> int:
-        n = n % 1000000007
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return self.fib(n - 1) + self.fib(n - 2)%1000000007
+        a, b, c = 0, 1, 0
+        if n < 2:
+            return n
+        while n >= 2:
+            c = a + b
+            a, b = b, c
+            n -= 1
+        return c % 1000000007
 
 
 if __name__ == '__main__':
-    res = Solution().fib(45)
-    print(res)
+    # 0 0
+    # 1 1
+    # 2 1
+    # 3 2
+
+    # res = Solution().fib(46) # 836311896
+    for _ in range(6):
+        res = Solution().fib(_)  # 5
+        print(_, res)
